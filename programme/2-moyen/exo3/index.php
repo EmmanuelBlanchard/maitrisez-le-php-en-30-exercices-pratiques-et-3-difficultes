732f10e4df1ccffc95c5b61e435b87e62a0f5a48
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-16 19:02:11 +0200

    Exercice 3 Moyen : Les tableaux associatifs

---
+++ programme/2-moyen/exo3/index.php
@@ -0,0 +1,87 @@
+<?php ob_start(); //NE PAS MODIFIER
+$titre = "Exo 3 : Les tableaux associatifs"; //Mettre le nom du titre de la page que vous voulez
+?>
+
+<!-- mettre ici le code -->
+<?php
+require("gestionTableau.php");
+
+$animal1 = [
+    "nom" => "Mina",
+    "age" => 2,
+    "type" => "chien"
+];
+$animal2 = [
+    "nom" => "Tya",
+    "age" => 7,
+    "type" => "chat"
+];
+$animal3 = [
+    "nom" => "Milo",
+    "age" => 4,
+    "type" => "chat"
+];
+$animal4 = [
+    "nom" => "Hocket",
+    "age" => 6,
+    "type" => "chien"
+];
+
+$tableauxDAnimaux = [
+    $animal1,
+    $animal2,
+    $animal3,
+    $animal4
+];
+
+function afficherAnimaux($tableauxDAnimaux){
+    echo "--------------------------- <br />";
+    foreach($tableauxDAnimaux as $tableauDAnimal) {
+        foreach($tableauDAnimal as $indice => $valeur) {
+            echo $indice . " : " .$valeur . "<br />";
+        }
+        echo "--------------------------- <br />";
+    }
+}
+
+function afficherAnimauxParType($tableauxDAnimaux,$type) {
+    echo "--------------------------- <br />";
+    foreach($tableauxDAnimaux as $tableauDAnimal) {
+        if($tableauDAnimal["type"] === $type) {
+            foreach($tableauDAnimal as $indice => $valeur) {
+                echo $indice . " : " .$valeur . "<br />";
+            }
+            echo "--------------------------- <br />";
+        }
+    }
+}
+?>
+
+<div>
+    <a href="?type=tous" class="btn btn-primary">Tous</a>
+    <a href="?type=chiens" class="btn btn-primary">Chiens</a>
+    <a href="?type=chats" class="btn btn-primary">Chats</a>
+</div>
+
+<?php
+    if(isset($_GET["type"]) && !empty($_GET["type"]) && $_GET["type"]!=="tout") {
+        if($_GET["type"] === "chiens") {
+            afficherAnimauxParType($tableauxDAnimaux,"chien");
+        } else if ($_GET["type"] === "chat") {
+            afficherAnimauxParType($tableauxDAnimaux,"chat");
+        } else {
+            afficherAnimaux($tableauxDAnimaux);
+        }
+    }
+?>
+
+<?php
+/************************
+ * NE PAS MODIFIER
+ * PERMET d INCLURE LE MENU ET LE TEMPLATE
+ ************************/
+    $content = ob_get_clean();
+    require "../../global/common/template.php";
+?>
+
+
