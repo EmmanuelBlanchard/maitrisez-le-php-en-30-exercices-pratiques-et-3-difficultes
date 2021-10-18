d8287c3741f5b36c69fd456b92ea7207772327db
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-18 19:10:54 +0200

    Cas pratique 3 : Mes animaux, La classe Animal

--- programme/4-cas pratique/3-Animaux/index.php
+++ programme/4-cas pratique/3-Animaux/index.php
@@ -5,13 +5,21 @@ $titre = "Les animaux"; //Mettre le nom du titre de la page que vous voulez
 <!-- mettre ici le code -->
 <?php
 require "MonPDO.class.php";
-$pdo = MonPDO::getPDO();
-$requete = "SELECT * FROM animal";
-$stmt = $pdo->prepare($requete);
-$stmt->execute();
-$animaux = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo "<pre>";
-print_r($animaux);
+require "Animal.class.php";
+// $pdo = MonPDO::getPDO();
+// $requete = "SELECT * FROM animal";
+// $stmt = $pdo->prepare($requete);
+// $stmt->execute();
+// $animaux = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// echo "<pre>";
+// print_r($animaux);
+
+// new Animal(1,"toto",23,true,"chien","");
+// new Animal(1,"titi",23,true,"chien","");
+// foreach(Animal::$mesAnimaux as $animal) {
+//     echo $animal->getNom() . "<br />";
+// }
+
 ?>
 
 <?php
